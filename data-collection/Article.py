4567c9f200620abb9ee7d43bf309c1fbd236cea9
lipinoelbreve <nicolupi.2@gmail.com>
2021-08-02 17:49:20 -0300

    Hago de cero el data collection

--- data-collection/Article.py
+++ data-collection/Article.py
@@ -1,23 +1,134 @@
-from dataclasses import dataclass
+from bs4 import BeautifulSoup
+import pickle as pk
+import requests
+import re
+import spacy
+nlp = spacy.load("en_core_web_sm")
+import geograpy
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
 
+def extract_affiliation_short_name(title):
+  words = re.split('[^\w\s\']', title)
+  
+  result = [word for word in words if re.search('unive', word.strip().lower())]
+  if len(result) > 0:
+    result = result[0].strip()
+  else:
+    result = [word for word in words if re.search('hosp', word.strip().lower())]
+    if len(result) > 0:
+      result = result[0].strip()
+    else:
+      result = [word for word in words if re.search('labor', word.strip().lower())]
+      if len(result) > 0:
+        result = result[0].strip()
+      else:
+        result = [word for word in words if re.search('insti', word.strip().lower())]
+        if len(result) > 0:
+          result = result[0].strip()
+        else:
+          result = [word for word in words if re.search('founda', word.strip().lower())]
+          if len(result) > 0:
+            result = result[0].strip()
+          else:
+            result = None
 
-@dataclass
+  return result
+
+class Author():
+  def __init__(self):
+    self.name = str
+    self.affiliation_long_name = str
+    self.affiliation_short_name = str
+    self.affiliation_country = str
+
 class Article():
-    authors: list(dict)
-    keywords: list
+  def __init__(self):
+    self.id = int
+    self.url = str
+    self.title = str
+    self.year = int
+    self.keywords = []
+    self.authors = []
 
-    def read_article():
-        pass
-
-@dataclass
 class ArticleCollection():
-    articles: dict(Article)
+  def __init__(self):
+    self.processed_pages = []
+    self.articles = dict()
+
+  def get_article_data(self, article_link):
+    no_keywords = False
+    no_authors = False
     
-    def save():
-        pass
+    r = requests.get(article_link)
+    souped = BeautifulSoup(r.content.decode('utf-8'), features='html.parser')
+    article = Article()
+    article.url = article_link
+    id = int(souped.find('strong', attrs={'title': 'PubMed ID'}).text)
+    article.id = id
+    article.title = souped.find('h1', attrs={'class': 'heading-title'}).text.strip()
 
-    def load():
-        pass
+    pub_date = souped.find('span', attrs={'class': 'cit'}).text.strip().split(';')[0]
+    article.year = int(re.findall('\d{4}', pub_date)[0])
 
-    def read_all_articles():
-        pass
+    authors_list = souped.find('div', attrs={'class': 'authors-list'})
+    authors = []
+    if authors_list != None:
+      authors_in_article = authors_list.find_all('span', attrs={'class': 'authors-list-item'})
+
+      for author_i in authors_in_article:
+        author = Author()
+        author_data = author_i.find('a', attrs={'class': 'full-name'})
+        affiliation_data = author_i.find_all('a', attrs={'class': 'affiliation-link'})
+
+        author.name = author_data['data-ga-label']
+        
+        if len(affiliation_data) > 0:
+          affiliation = affiliation_data[0] # En caso de tener más de 1 afiliación, tomamos la primera
+          title = affiliation['title']
+          author.affiliation_long_name = title
+          doc = nlp(title)
+
+          author.affiliation_short_name = extract_affiliation_short_name( title )
+
+          places = [re.sub('[^\w\s]', '', str(ent)) for ent in doc.ents if ent.label_ == 'GPE' ]
+          places = ['United States' if place =='USA' else place for place in places]
+          places = ['United Kingdom' if place =='UK' else place for place in places]
+          places = geograpy.places.PlaceContext(places)
+          if len(places.countries) > 0:
+            author.affiliation_country = places.countries[0]
+          elif len(places.other) > 0:
+            author.affiliation_country = places.other[0]
+          
+        authors.append(author)
+      article.authors = authors
+    if len(authors) == 0:
+      no_authors = True
+
+    keywords = souped.find('strong', string='\n          Keywords:\n        ')
+    if keywords != None:
+      keywords = keywords.next_sibling.strip().split(';')
+      keywords = [re.sub( ' +', ' ', re.sub('[^\w\s]', ' ', keyword.strip()) ) for keyword in keywords ]
+      article.keywords = keywords
+    else:
+      no_keywords = True
+
+    if no_keywords:
+      return 'skipped - no keywords'
+    elif no_authors:
+      return 'skipped - no authors'
+    else:
+      self.articles[id] = article
+      return 'passed'
+        
+  def save(self, filename):
+    with open(filename, 'wb') as output:
+      pk.dump(self.__dict__, output, pk.HIGHEST_PROTOCOL)
+
+  def load(self, filename):
+    with open(filename, 'rb') as input:
+      tmp_dict = pk.load(input)
+    self.__dict__.update(tmp_dict)
--- data-collection/main.py
+++ data-collection/main.py
@@ -13,14 +13,13 @@
 # frenar en cualquier momento
 
 from bs4 import BeautifulSoup
-import pickle as pk
 import requests
 import re
 import numpy as np
-from datetime import datetime
 from tqdm import tqdm
 from time import sleep
 import os
+from Article import ArticleCollection, Article, Author
 
 #%%
 base_url = 'https://pubmed.ncbi.nlm.nih.gov'
@@ -29,118 +28,20 @@ filter_url = '/?term=(((%222016%22%5BDate%20-%20Publication%5D%20%3A%20%223000%2
 max_pages = 1000
 pause_duration = 5 # segundos entre requests
 
-#%%
-class generic_class():
-  def __init__(self):
-    pass
-  def load(self, filename):
-    with open(filename, 'rb') as input:
-      tmp_dict = pk.load(input)
-    self.__dict__.update(tmp_dict)
-
-class articles_data():
-  def __init__(self):
-    pass
-  def store(self, processed_pages, articles):
-    self.processed_pages = processed_pages
-    self.articles = articles
-  def write(self, filename):
-    with open(filename, 'wb') as output:
-      pk.dump(self.__dict__, output, pk.HIGHEST_PROTOCOL)
-
-articles = generic_class()
 
 #%%
 # Si ya existe articles.pkl lo cargo y avanzo desde la última página
 # Si no existe creo el dict() de cero
 current = os.getcwd()
 filename = current + '/articles.pkl'
+article_collection = ArticleCollection()
 if os.path.exists(filename):
-  articles.load('articles.pkl')
-  processed_pages = articles.processed_pages
-  remaining_pages = np.arange(processed_pages[-1], max_pages+1)
-  articles = articles.articles
+  article_collection.load('articles.pkl')
+  remaining_pages = np.arange(article_collection.processed_pages[-1], max_pages+1)
 else:
-  articles = dict()
-  processed_pages = []
   remaining_pages = np.arange(1,max_pages+1)
 
 #%%
-def get_article_data(article_link):
-  r = requests.get(article_link)
-  souped = BeautifulSoup(r.content.decode("utf-8"), features="html.parser")
-  article = dict()
-
-  article['url'] = article_link
-  article['id'] = int(souped.find('strong', attrs={'title': 'PubMed ID'}).text)
-
-  title = souped.find('h1', attrs={'class': 'heading-title'}).text.strip()
-  article['title'] = title
-
-  pub_date = souped.find('span', attrs={'class': 'cit'}).text.strip().split(';')[0]
-
-  try: # Intento guardar toda la información de la fecha (día mes año).. Si no es posible solo guardo el año
-    pub_date = datetime.strptime(pub_date, '%Y %b %d').date()
-
-    article['pub_date'] = pub_date
-    article['pub_year'] = pub_date.year
-  except:
-    article['pub_year'] = int(re.findall('\d{4}', pub_date)[0])
-
-  
-  # Guardo autores en un diccionario, con nombre e institución (afilliation), si hay autores
-  
-  authors_list = souped.find('div', attrs={'class': 'authors-list'})
-  if authors_list != None:
-    authors = []
-    authors_in_article = authors_list.find_all('span', attrs={'class': 'authors-list-item'})
-
-    for author_i in authors_in_article:
-      author = dict()
-      author_data = author_i.find('a', attrs={'class': 'full-name'})
-
-      affiliation_data = author_i.find_all('a', attrs={'class': 'affiliation-link'})
-
-      author['name'] = author_data['data-ga-label']
-
-      if len(affiliation_data) > 0:
-        author_affiliation = []
-        for affiliation in affiliation_data:
-          affiliation_dict = dict()
-          affiliation_dict['affiliation'] = affiliation['title']
-          affiliation_dict['country'] = re.sub('[^\w\s]', '', affiliation['title'].split(',')[-1].strip())
-          author_affiliation.append( affiliation_dict )
-        author['affiliation'] = author_affiliation
-      
-      authors.append( author )
-
-    article['authors'] = authors
-
-  # Guardo abstract si hay
-  abstract = souped.find('div', attrs={'class': 'abstract-content selected'})
-  if abstract != None:
-    abstract = abstract.find('p').text.strip()
-    article['abstract'] = re.sub(' +', ' ', re.sub('\n', '', abstract))
-
-  # Guardo Keywords si hay
-  keywords = souped.find('strong', string='\n          Keywords:\n        ')
-  if keywords != None:
-    keywords = keywords.next_sibling.strip().split(';')
-    keywords = [re.sub('[^\w\s]', '', keyword.strip()) for keyword in keywords]
-    article['keywords'] = keywords
-  
-  # Guardo Mesh terms si hay
-  mesh_terms = souped.find('div', attrs={'id': 'mesh-terms'})
-  if mesh_terms != None:  
-    mesh_terms = mesh_terms.find_all('button')
-    mesh_terms = [ re.sub('[^\w\s/]', '', mesh_term.text.strip()) for mesh_term in mesh_terms]
-    article['mesh_terms'] = mesh_terms
-
-  return article
-
-#%%
-stored_articles = articles_data()
-
 print('Descargando articulos...')
 print('Ctrl + C para frenar (todo el proceso es guardado)')
 
@@ -150,18 +51,16 @@ for page in tqdm( remaining_pages ):
   souped = BeautifulSoup(r.content.decode("utf-8"), features="html.parser")
 
   articles_in_page = souped.find_all('a', attrs={'class': 'docsum-title'})
-  articles_ids = [ re.sub('^[\d]', '', article['href']) for article in articles_in_page ]
+  articles_ids = [ int(re.sub('[^\d]', '', article['href'])) for article in articles_in_page ]
   
   for article_id in articles_ids:
-    if article_id not in articles.keys():
-      article_link = base_url + '/' + article_id
-      article = get_article_data( article_link )
-    
-      articles[ article_id ] = article
-      stored_articles.store(processed_pages, articles)
-      stored_articles.write('articles.pkl')
-      print('Agregado artículo', article['id'])
+    if article_id not in article_collection.articles.keys():
+      article_link = base_url + '/' + str(article_id)
+      res = article_collection.get_article_data( article_link )
+      article_collection.save('articles.pkl')
+      print(res, article_id)
       sleep(pause_duration)
 
-  if page not in processed_pages:
-    processed_pages.append(page)
+  if page not in article_collection.processed_pages:
+    article_collection.processed_pages.append(page)
+    print('Processed page', page)
